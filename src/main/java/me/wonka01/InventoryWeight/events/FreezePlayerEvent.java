3e8c71337289359cad78d0d24446c8647105fc05
inhurstory <inhurstory@gmail.com>
2025-11-18 22:27:27 +0800

    Bug fix: fix disableMovement can't disable jump

--- src/main/java/me/wonka01/InventoryWeight/events/FreezePlayerEvent.java
+++ src/main/java/me/wonka01/InventoryWeight/events/FreezePlayerEvent.java
@@ -15,7 +15,7 @@ public class FreezePlayerEvent implements Listener {
 
     @EventHandler
     public void onPlayerMove(PlayerMoveEvent e) {
-        if (!WorldList.getInstance().isInventoryWeightEnabled(e.getPlayer().getWorld().toString())) {
+        if (!WorldList.getInstance().isInventoryWeightEnabled(e.getPlayer().getWorld().getName())) {
             return;
         }
 
